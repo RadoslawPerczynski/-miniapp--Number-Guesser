3cb2290870ad6d796a763c1def706378d42c95bc
Radoslaw Perczynski <perczynski.radoslaw@gmail.com>
2018-12-12 22:32:18 +0100

    finishing - random number finalized and in progress of handling duplicates

--- app.js
+++ app.js
@@ -2,58 +2,79 @@
 let guessInput = document.querySelector('#number-input'),
   minField = document.querySelector('.min'),
   maxField = document.querySelector('.max'),
+  attemptsField = document.querySelector('.attempts')
   btn = document.querySelector('input[type="submit"]'),
   container = document.querySelector('.container'),
   messageParagraph = document.querySelector('.message');
 
 
   //game
-
   let min = 2,
-  max = 11,
-  btnText = btn.value,
-  winningNumber = 3,
-  numberOfAttempts = 3;
+    max = 8,
+    winningNumber = getRandomNumber(min, max),
+    numberOfAttempts = 3,
+    userAttempts = [];
 
 minField.textContent = min;
 maxField.textContent = max;
+attemptsField.textContent = numberOfAttempts;
+
 
 btn.addEventListener('click', processTheGuess);
+showIfGuessNumberDuplicated();
+
+
+function showIfGuessNumberDuplicated() {
+  guessInput.addEventListener('input', function() {
+
+    let CurrentNumber = parseInt(guessInput.value);
+    let CurrentAttempts = userAttempts;
+    console.log(CurrentNumber);
+    console.log(CurrentAttempts);
+    console.log(CurrentAttempts.indexOf(CurrentNumber))
+
+    if(CurrentAttempts.indexOf(CurrentNumber) === -1) {
+      console.log('allowed')
+    } else {
+      console.log('not allowerd')
+    }
+
+   
+})
+
+}
 
 function processTheGuess(e) {
   e.preventDefault();
+
   let guessValue = parseInt(guessInput.value);
 
   if(isNaN(guessValue) || guessValue > max || guessValue < min) {
-    return setMessage(false, `Incorrect input. Your number must be between ${min} and ${max}`)
+    guessInput.focus();
+    return setMessage(false, `Incorrect input. Your number must be between ${min} and ${max}`); 
   }
-
 
   if(guessValue === winningNumber) {
     winNotification()
   }
   else {
-    processAttempt();
-
-
-    if(numberOfAttempts === 0) {
-      gameOver();
-      
-    }
+    userAttempts.push(guessValue);
+    goToNextAttempt();
   }
 
 }
 
 function winNotification() {
   setMessage(true, 'Congrats! ;) You won!')
-  btn.disabled = true;
   guessInput.disabled = true;
   playAgain()
 }
-function processAttempt() {
-  numberOfAttempts--;
-  setMessage(false, `Fail. ${numberOfAttempts} attempts left.`)
+function goToNextAttempt(e) {
 
+  numberOfAttempts--;
+  guessInput.value = "";
+  guessInput.focus();
+  setMessage(false, `Fail. ${numberOfAttempts} attempts left.`)
   if(numberOfAttempts === 0) {
     gameOver()
 
@@ -61,23 +82,35 @@ function processAttempt() {
 
 }
 function gameOver() {
-  setMessage(false, `Game over. The correct number was ${winningNumber}`);
-
+  setMessage(false, `Game over. The correct number was ${winningNumber}. Your numbers: ${userAttempts.join(' | ')}`);
+  guessInput.disabled = true;
   playAgain();
 }
 
 function setMessage(win, message) {
 
   win===true ? messageParagraph.style.color = 'green' : messageParagraph.style.color = 'red'
-
   win===true ? guessInput.style.borderColor = 'green' : guessInput.style.borderColor = 'red'
-
   messageParagraph.textContent = message;
 
 }
 function playAgain() {
-  
-}
+  btn.value = 'Play again';
+  const playAgainClass = 'play-again';
 
+  btn.classList.add(playAgainClass);
+  let playAgainButton = document.querySelector("."+playAgainClass);
+  btn.removeEventListener('click', processTheGuess);
+
+  playAgainButton.addEventListener('click', function() {
+    window.location.reload();
+  })
+
+}
+function getRandomNumber(min, max) {
+  let randomNum = Math.random()*(max-min+1)+min;
+
+  return Math.floor(randomNum);
+}
 
     
